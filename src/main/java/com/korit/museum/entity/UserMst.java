aba69da8f989329c9a2f5c14fe6a97e0bba34bca
1nyeong <inyeong0726@gmail.com>
2023-02-16 18:24:17 +0900

    Account 2

--- src/main/java/com/korit/museum/entity/UserMst.java
+++ src/main/java/com/korit/museum/entity/UserMst.java
@@ -1,4 +1,26 @@
 package com.korit.museum.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
 public class UserMst {
+
+    private String userId;
+
+    private String username;
+
+    private String password;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
 }
