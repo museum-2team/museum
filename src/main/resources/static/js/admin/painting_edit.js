e35b70166cde7aedbd9656097c88fcad7612ab40
1nyeong <inyeong0726@gmail.com>
2023-03-06 19:47:35 +0900

    js

--- src/main/resources/static/js/admin/painting_edit.js
+++ src/main/resources/static/js/admin/painting_edit.js
@@ -150,5 +150,259 @@ class PaintingEditService {
     setPaintingObjValues() {
         const editInputs = document.querySelectorAll(".edit-input");
 
+        paintingObj.paintingCode = editInputs[0].value;
+        paintingObj.paintingTitleName = editInputs[1].value;
+        paintingObj.exhibitionWorks = editInputs[2].value;
+        paintingObj.viewingTime = editInputs[3].value;
+        paintingObj.paintingName = editInputs[4].value;
+        paintingObj.author = editInputs[5].value;
+        paintingObj.paintingSize = editInputs[6].value;
+        paintingObj.year_of_Manufacture = editInputs[7].value;
+        paintingObj.material = editInputs[8].value;
+
+    }
+
+    loadPaintingAndImageData(){
+        const responseData = PaintingEditApi.getInstance().getPaintingAndImage();
+
+        if(responseData.paintingMst == null){
+            alert("해당 작품코드는 등록되지 않은 코드입니다.");
+            history.back();
+            return;
+        }
+
+        const editInputs = document.querySelectorAll(".edit-input");
+        editInputs[0].value = responseData.paintingMst.paintingCode;
+        editInputs[1].value = responseData.paintingMst.paintingTitleName;
+        editInputs[2].value = responseData.paintingMst.exhibitionWorks;
+        editInputs[3].value = responseData.paintingMst.viewingTime;
+        editInputs[4].value = responseData.paintingMst.paintingName;
+        editInputs[5].value = responseData.paintingMst.author;
+        editInputs[6].value = responseData.paintingMst.paintingSize;
+        editInputs[7].value = responseData.paintingMst.year_of_Manufacture;
+        editInputs[8].value = responseData.paintingMst.material;
+
+        if(responseData.paintingImage != null){
+            imgObj.imageId = responseData.paintingImage.imageId;
+            imgObj.paintingCode = responseData.paintingImage.paintingCode;
+            imgObj.saveName = responseData.paintingImage.saveName;
+            imgObj.originName = responseData.paintingImage.originName;
+
+            const paintingImage = document.querySelector(".painting-img");
+            paintingImage.src = `http://localhost:8000/image/painting/${responseData.paintingImage.saveName}`;
+        }
+    }
+
+    loadCategories(){
+        const responseData = PaintingEditApi.getInstance().getCategories();
+
+        const categorySelect = document.querySelector(".category-select");
+
+        categorySelect.innerHTML = `<option value="">전체조회</option>`
+
+        responseData.forEach(data => {
+            categorySelect.innerHTML += `
+                <option value = "${data.category}">${data.category}</option>
+            `;
+        });
+    }
+
+    setErrors(error){
+        const errorMessages = document.querySelectorAll(".error-message");
+        this.clearErrors();
+
+        Object.keys(errors).forEach(key => {
+            if(key == "paintingCode") {
+                errorMessages[0].innerHTML = errors[key];
+            }else if(key == "paintingTitmeName") {
+                errorMessages[1].innerHTML = errors[key];
+            }else if(key == "exhibitionWorks") {
+                errorMessages[2].innerHTML = errors[key];
+            }else if(key == "viewingTime") {
+                errorMessages[3].innerHTML = errors[key];
+            }else if(key == "paintingName") {
+                errorMessages[4].innerHTML = errors[key];
+            }else if(key == "paintingSize") {
+                errorMessages[6].innerHTML = errors[key];
+            }else if(key == "material") {
+                errorMessages[7].innerHTML = errors[key];
+            }
+        })
+    }
+
+    clearErrors(){
+        const errorMessages = document.querySelectorAll(".error-message");
+        errorMessages.forEach(error => {
+            error.innerHTML = "";
+        })
     }
 }
+
+class ImgFileService {
+    static #instance = null;
+    static getInstance(){
+        if(this.#instance == null){
+            this.#instance = new ImgFileService();
+        }
+        return this.#instance;
+    }
+
+    getImgPreview(){
+        const painitngImg = document.querySelector(".painting-img");
+
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            painitngImg.src = e.target.result;
+        }
+
+        reader.readAsDataURL(fileObj.files[0]);
+    }
+}
+
+class ComponentEvent{
+    static #instance = null;
+    static getInstance(){
+        if(this.#instance == null){
+            this.#instance = new ComponentEvent
+        }
+        return this.#instance;
+    }
+
+    addClickEventEditButton(){
+        const editButton = document.querySelector(".edit-button");
+
+        editButton.onclick = () =>{
+            PaintingEditService.getInstance().setPaintingObjValues();
+            const successFlag = PaintingEditApi.getInstance().modifyPainting();
+
+            if(!successFlag){
+                return;
+            }
+
+            PaintingEditService.getInstance().clearErrors();
+
+            if(confirm("이미지를 수정하시겠습니까?")){
+                const imgAddButton = document.querySelector(".img-add-button");
+                const imgCancelButton = document.querySelector(".img-cancel-button");
+
+                imgAddButton.disabled = false;
+                imgCancelButton.disabled = false;
+            }else{
+                location.reload();
+            }
+        }
+    }
+
+    addClickEventImgAddButton(){
+        const imgFile = document.querySelector(".img-file");
+        const addButton = document.querySelector(".img-add-button");
+
+        addButton.onload = () => {
+            imgFile.click();
+        }
+    }
+
+    addChangeEventImgFile(){
+        const imgFile = document.querySelector(".img-file");
+
+        imgFile.onchange = () =>{
+            const formData = new FormData(document.querySelector(".img-form"));
+            let changeFlag = false;
+
+            fileObj.files.pop();
+
+            formData.forEach(value => {
+                fileObj.files.push(value);
+                changeFlag = true;
+            });
+
+            if(changeFlag){
+                const imgEditButton = document.querySelector(".img-edit-button");
+                imgEditButton.disabled = false;
+
+                ImgFileService.getInstance().getImgPreview();
+                imgFile.value = null;
+            }
+        }
+    }
+
+    addClickEventImgEditButton(){
+        const imgEditButton = document.querySelector(".img-edit-button");
+
+        imgEditButton.onclick = () =>{
+            fileObj.formData.append("files", fileObj.files[0]);
+
+            let successFlag = true;
+
+            if(imgObj.imageId != null){
+                successFlag = PaintingEditApi.getInstance().removeImg();
+            }
+
+            if(successFlag){
+                PaintingEditApi.getInstance().addImg();
+            }
+        }
+    }
+    
+    addClickEventImgCancelButton(){
+        const imgCancelButton = document.querySelector(".img-cancel-button");
+
+        imgCancelButton.onclick = () =>{
+            if(confirm("이미지를 수정하시겠습니까?")){
+                location.reload();
+            }
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
